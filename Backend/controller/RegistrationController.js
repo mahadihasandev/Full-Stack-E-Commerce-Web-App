9cc34afd1090a9f642b43e2dcbe6ae44bace1897
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-10-23 14:09:09 +0600

    Name email and pasword validation

--- Backend/controller/RegistrationController.js
+++ Backend/controller/RegistrationController.js
@@ -1,4 +1,23 @@
+let emailRegex=require('../helper/EmailRegex')
+let PasswordRegex=require('../helper/PasswordRegex')
+let PasswordRegexEight=require('../helper/PasswordRegexEight')
+
 let Registration=(req,res)=>{  
-    res.send("registration")
+    let {username,email,password}=req.body
+    if(!username){
+        res.send({error:"Pleaser Enter a user name"})
+    }else if(!email){
+        res.send({error:"Pleaser Enter a email"})
+    }else if(!password){
+        res.send({error:"Pleaser Enter a password"})
+    }else if(!emailRegex(email)){
+       res.send({error:"Enter a valid email"})
+    }else if(!PasswordRegex(password)){
+        res.send({error:"enter a at least 1 digit" })
+    }else if(!PasswordRegexEight(password)){
+          res.send({error:"enter a at least 8 character" })
+    }else{
+        res.send({Success:"account created"})
+    }
 }
 module.exports=Registration
--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const router=require('./router')
 
+app.use(express.json())
 app.use(router)
 
 app.listen(port, () => {
