063ca183a4640cb3159455be674b1c8f7f68d550
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-11-18 23:45:59 +0600

    in DashboardMenu and side bar added

--- Dashboard/Layouts/Home.jsx
+++ Dashboard/Layouts/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Col, Row } from 'antd';
 import { Outlet } from 'react-router-dom';
+import DashboardMenu from '../Components/DashboardMenu';
 
 function Home() {
   return (
     <div>
       <Row>
-      <Col span={6}>col-12</Col>
+      <Col span={6}>
+      <DashboardMenu/>
+      </Col>
       <Col span={18}>
       <Outlet/>
       </Col>
