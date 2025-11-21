cd21c6b58c21762ca77ef28d2e0f33b225a69d3d
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-11-21 21:32:02 +0600

    Data sended to Category collec6tion in mongoDB

--- Backend/controller/AddCategoryController.js
+++ Backend/controller/AddCategoryController.js
@@ -1,7 +1,19 @@
+const CategorySchema = require("../model/CategorySchema");
+
+
 const AddCategoryController=(req,res)=>{
     let {name,ownerId}=req.body
-    console.log(name);
-    console.log(ownerId);
+
+let existCategory=CategorySchema.find({name:name})
+if(existCategory){
+ res.send({error:"Category Already Exist"})
+}else{
+    let categoryData=new CategorySchema({
+    name:name,
+    ownerId:ownerId,
+})
+categoryData.save()
+}
 }
 
 module.exports=AddCategoryController
